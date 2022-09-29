1df14a1a29e2bdd645e907734eb226515a2fafc6
Wu chao <chao.wu@bitdeer.com>
2022-09-29 16:14:39 +0800

    feature: DP-32 - update the healthcheck endpoint for the jira connector proxy (for future purpose)

--- src/init.ts
+++ src/init.ts
@@ -1,17 +1,18 @@
 import express, { Express, Request, Response } from 'express';
 
-import JiraClient  from 'jira-connector';
-
-
 const app: Express = express();
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000. Initialization completed...')
 })
 
-app.get("/test", (req: Request, res: Response) => {
+app.get("/health", (req: Request, res: Response) => {
   console.log(req.query);
+
   res.send({
-    status: "success"
+    code: 0,
+    status: "success",
   })
 });
+
+export default app;
